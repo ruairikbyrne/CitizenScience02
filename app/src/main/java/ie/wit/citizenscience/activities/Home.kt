571470d06b7521a230069f94e74549cc286f5c0c
Ruairi Byrne <ruairikbyrne@hotmail.com>
2021-11-30 11:03:41 +0000

    Complete Navgraph

--- app/src/main/java/ie/wit/citizenscience/activities/Home.kt
+++ app/src/main/java/ie/wit/citizenscience/activities/Home.kt
@@ -8,30 +8,39 @@ import ie.wit.citizenscience.R
 import ie.wit.citizenscience.databinding.HomeBinding
 import androidx.navigation.*
 import androidx.navigation.ui.*
+import timber.log.Timber.i
 
 class Home : AppCompatActivity() {
 
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var homeBinding : HomeBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
         homeBinding = HomeBinding.inflate(layoutInflater)
         setContentView(homeBinding.root)
         drawerLayout = homeBinding.drawerLayout
-
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         val navController = findNavController(R.id.nav_host_fragment)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
 
-        setContentView(R.layout.home)
-        //val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        //setSupportActionBar(toolbar)
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.sightingFragment, R.id.sightingListFragment), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        val navView = homeBinding.navView
+        navView.setupWithNavController(navController)
+        //setContentView(R.layout.home)
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
-        return NavigationUI.navigateUp(navController, drawerLayout)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
